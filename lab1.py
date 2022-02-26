3084f1cff1a2afd364d1a6daa528084487c544b4
ZhekLu <justletmego@list.ru>
2022-02-26 23:20:06 +0300

    Logic complete

---
+++ lab1.py
@@ -0,0 +1,17 @@
+from text_info import TextInfo
+
+
+def main():
+    print("How many words should be in top: ")
+    top_num = int(input())
+    print("Anagram size: ")
+    ng_size = int(input())
+    with open('./data/input.txt', 'r') as f:
+        text = f.read()
+        ti = TextInfo(text)
+        print(f"Median: {ti.get_median()}")
+        print(f"Average: {ti.get_mean()}")
+        print(f"Top: {ti.get_top_ngrams(top_num, ng_size)}")
+
+
+main()
--- text_info.py
+++ text_info.py
@@ -5,19 +5,14 @@ import re
 
 class TextInfo:
     def __init__(self, text: str):
-        self.text = text.lower
+        self.text = text.lower()
         self.__prepare()
 
     def __prepare(self):
         sentences = [i for i in re.split(r'[.!?;]', self.text) if i != '']
-        sentences = list(map(lambda x: [i for i in re.split(r'\W', x) if i != ''], sentences))
+        self.sentences = list(map(lambda x: [i for i in re.split(r'\W', x) if i != ''], sentences))
 
-        self.words_num = [len(i) for i in sentences]
-
-        c = Counter()
-        for sentence in sentences:
-            c.update(Counter(sentence))
-        self.words_dict = dict(c)
+        self.words_num = [len(i) for i in self.sentences]
 
     def set_text(self, text: str):
         self.__init__(text)
@@ -27,3 +22,13 @@ class TextInfo:
 
     def get_median(self):
         return statistics.median(self.words_num)
+
+    def get_top_ngrams(self, top_size: int, ngram_size: int):
+        c = Counter()
+        for sentence in self.sentences:
+            for word in sentence:
+                if len(word) < ngram_size:
+                    continue
+                for i in range(len(word) - ngram_size + 1):
+                    c[word[i:i + ngram_size]] += 1
+        return c.most_common(top_size)
