a594e819124a99e55d59d6d133e48ccc58a2f66e
ZhekLu <justletmego@list.ru>
2022-03-01 20:45:34 +0300

    Fix arch: add property

--- code/text_info.py
+++ code/text_info.py
@@ -1,31 +1,37 @@
-import statistics
-from collections import Counter
 import re
+import statistics
+
+from collections import Counter
 
 
 class TextInfo:
     def __init__(self, text: str):
+        self._sentences = None
+        self._words_num = None
         self.text = text.lower()
-        self.__prepare()
 
-    def __prepare(self):
-        sentences = [i for i in re.split(r'[.!?;]', self.text) if i != '']
-        self.sentences = list(map(lambda x: [i for i in re.split(r'\W', x) if i != ''], sentences))
+    @property
+    def text(self):
+        return self._text
 
-        self.words_num = [len(i) for i in self.sentences]
+    @text.setter
+    def text(self, value):
+        self._text = value
 
-    def set_text(self, text: str):
-        self.__init__(text)
+        sentences = [i for i in re.split(r'[.!?;]', value) if i != '']
+        self._sentences = list(map(lambda x: [i for i in re.split(r'\W', x) if i != ''], sentences))
+
+        self._words_num = [len(i) for i in self._sentences]
 
     def get_mean(self):
-        return statistics.mean(self.words_num)
+        return statistics.mean(self._words_num)
 
     def get_median(self):
-        return statistics.median(self.words_num)
+        return statistics.median(self._words_num)
 
-    def get_top_ngrams(self, top_size: int, ngram_size: int):
+    def get_top_ngrams(self, top_size: int, ngram_size: int) -> list:
         c = Counter()
-        for sentence in self.sentences:
+        for sentence in self._sentences:
             for word in sentence:
                 if len(word) < ngram_size:
                     continue
