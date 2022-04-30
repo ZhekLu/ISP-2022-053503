03909eb563e5b02dbdd4f9eac9a078bdc0f90a61
ZhekLu <justletmego@list.ru>
2022-04-30 16:41:21 +0300

    Merge

--- serializer/test/__init__.py
+++
@@ -1,7 +0,0 @@
-from test.testers.json_test import JsonSerializerTester
-from test.testers.toml_test import TomlSerializerTester
-from test.testers.yaml_test import YamlSerializerTester
-
-json = JsonSerializerTester
-toml = TomlSerializerTester
-yaml = YamlSerializerTester
