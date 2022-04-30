e2bd4ff0735edb0ab18004d1e366104d2365206b
ZhekLu <justletmego@list.ru>
2022-04-30 19:52:08 +0300

    Final?

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ISP-2022-053503
-Rotkovich E.
+# ISP LABS
+Rotkovich E. 053503
+
+## Serializer
+[![Serializer Actions Status](https://github.com/ZhekLu/ISP-2022-053503/workflows/test-serializers/badge.svg)](https://github.com/ZhekLu/ISP-2022-053503/actions)
+[![codecov](https://codecov.io/gh/ZhekLu/ISP-2022-053503/branch/main/graph/badge.svg?token=TZPW485XC8)](https://codecov.io/gh/ZhekLu/ISP-2022-053503)
--- serializer-project/test/testers/json_test.py
+++ serializer-project/test/testers/json_test.py
@@ -1,16 +1,7 @@
 from serializer import JSON_STR
 from test.testers import SerializerTester
-import unittest
 
 
 class JsonSerializerTester(SerializerTester):
     TEST_FILE = 'tester_file.json'
     SERIALIZER_STR = JSON_STR
-
-
-if __name__ == '__main__':
-    suite = unittest.TestSuite()
-    suite.addTest(JsonSerializerTester())
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    # unittest.main()
-    # TODO! why all tests ran? @@unittest.skipIf() - skip all
